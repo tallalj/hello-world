2eaa01029b34fc70c4dc4ab4d34b8bf103011b54
Tallal Javed <Tallal.javed@outlook.com>
2020-11-17 14:49:47 +0500

    Meh

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <button @click="onClick">Click me Tabish</button>
   </div>
 </template>
 
@@ -13,6 +14,11 @@ export default {
   name: "Home",
   components: {
     HelloWorld
+  },
+  methods:{
+    onClick(){
+      this.$router.push('About')
+    }
   }
 };
 </script>
